feb0686ca77f90a4744a12cab24197eb6998d44f
Bicycle Tangerine <RanZH-47>
2023-01-28 00:03:43 +0800

    chore: rename based on go style

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ import (
 
 func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
-	hh := handler.HomeHandler(l)
+	hh := handler.NewHomeHandler(l)
 	gb := handler.NewGoodbyeHandler(l)
 
 	sm := http.NewServeMux()
